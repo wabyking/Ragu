0c9eae2aa8d8cbca913e3f319ecf2e2748e44555
waby <810553967@qq.com>
2019-08-12 12:33:43 +0200

    Update attacker.py

--- Ragu/attacker.py
+++ Ragu/attacker.py
@@ -1,8 +1,8 @@
 class Attacker(object):
 	"""docstring for Attacker"""
-	def __init__(self, arg):
+	def __init__(self, task):
 		super(Attacker, self).__init__()
-		self.arg = arg
+		self.arg = task
 
 	def attack(self,sentence):
 		return "placeholder, please use specific Attacker instead"
@@ -10,9 +10,9 @@ class Attacker(object):
 
 class Ragu(Attacker):
 	"""docstring for Ragu"""
-	def __init__(self, arg):
-		super(Ragu, self).__init__()
-		self.arg = arg
+	def __init__(self, task):
+		super(Ragu, self).__init__(task)
+
 
 	def attack(self,sentence):
 		sentence = sentence + "- attacked"
